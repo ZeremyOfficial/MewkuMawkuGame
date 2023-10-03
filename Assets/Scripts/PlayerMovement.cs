563c0ec12338a39c6e4b8eed9cdbf5f93615f8ee
Zeremy <122979962+ZeremyFAT@users.noreply.github.com>
2023-10-03 17:56:16 +0300

    SwordAttack fixed?

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,13 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D myRB;
     private Animator myAnim;
     private bool isAttacking = false;
-    private float attackCooldown = 0.5f; // Adjust this value as needed
+    private bool isCooldown = false; // Added a flag for cooldown
+    private float attackCooldown = 1f; // Adjust this value as needed
     private float attackCooldownTimer = 0f;
+    private float attackDuration = 0.5f; // Adjust this value as needed
+    private float attackDurationTimer = 0f;
+    private float attackSpeedMultiplier = 0.3f; // Adjust this value to control attack speed
+    private float originalSpeed; // Store the original speed
 
     [SerializeField]
     private float speed;
@@ -17,6 +22,7 @@ public class PlayerMovement : MonoBehaviour
     {
         myRB = GetComponent<Rigidbody2D>();
         myAnim = GetComponent<Animator>();
+        originalSpeed = speed;
     }
 
     void Update()
@@ -33,7 +39,7 @@ public class PlayerMovement : MonoBehaviour
         Vector2 inputVector = new Vector2(horizontalInput, verticalInput).normalized;
 
         // Apply velocity directly to the rigidbody
-        myRB.velocity = inputVector * speed;
+        myRB.velocity = inputVector * (isAttacking ? speed * attackSpeedMultiplier : speed);
 
         myAnim.SetFloat("moveX", inputVector.x);
         myAnim.SetFloat("moveY", inputVector.y);
@@ -47,53 +53,68 @@ public class PlayerMovement : MonoBehaviour
 
     void HandleAttackInput()
     {
-        // Check if the attack cooldown timer has elapsed
-        if (attackCooldownTimer <= 0)
+        // Check if the attack duration has elapsed
+        if (isAttacking && attackDurationTimer > 0)
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                // Determine the direction the character is facing
-                float lastMoveX = myAnim.GetFloat("lastMoveX");
-                float lastMoveY = myAnim.GetFloat("lastMoveY");
+            attackDurationTimer -= Time.deltaTime;
+        }
 
-                // Set the appropriate trigger based on direction
-                if (Mathf.Abs(lastMoveX) > Mathf.Abs(lastMoveY))
+        // Check if the attack cooldown has elapsed
+        if (isCooldown && attackCooldownTimer > 0)
+        {
+            attackCooldownTimer -= Time.deltaTime;
+        }
+
+        // Check for attack input and that the player is not already attacking and the cooldown has elapsed
+        if (Input.GetKeyDown(KeyCode.Space) && !isAttacking && attackCooldownTimer <= 0)
+        {
+            // Determine the direction the character is facing
+            float lastMoveX = myAnim.GetFloat("lastMoveX");
+            float lastMoveY = myAnim.GetFloat("lastMoveY");
+
+            // Set the appropriate trigger based on direction
+            if (Mathf.Abs(lastMoveX) > Mathf.Abs(lastMoveY))
+            {
+                // Character is primarily moving horizontally
+                if (lastMoveX > 0)
                 {
-                    // Character is primarily moving horizontally
-                    if (lastMoveX > 0)
-                    {
-                        myAnim.Play("AttackRight", -1, 0f); // Play attack animation immediately
-                    }
-                    else
-                    {
-                        myAnim.Play("AttackLeft", -1, 0f); // Play attack animation immediately
-                    }
+                    myAnim.Play("AttackRight", -1, 0f); // Play attack animation immediately
                 }
                 else
                 {
-                    // Character is primarily moving vertically
-                    if (lastMoveY > 0)
-                    {
-                        myAnim.Play("AttackUp", -1, 0f); // Play attack animation immediately
-                    }
-                    else
-                    {
-                        myAnim.Play("AttackDown", -1, 0f); // Play attack animation immediately
-                    }
+                    myAnim.Play("AttackLeft", -1, 0f); // Play attack animation immediately
+                }
+            }
+            else
+            {
+                // Character is primarily moving vertically
+                if (lastMoveY > 0)
+                {
+                    myAnim.Play("AttackUp", -1, 0f); // Play attack animation immediately
+                }
+                else
+                {
+                    myAnim.Play("AttackDown", -1, 0f); // Play attack animation immediately
                 }
 
-                isAttacking = true;
-                attackCooldownTimer = attackCooldown; // Reset the cooldown timer
             }
+            isAttacking = true;
+            isCooldown = true;
+            attackDurationTimer = attackDuration;
+            speed *= attackSpeedMultiplier; // Reduce speed during attack
         }
-        else
+
+        // Check if the attack animation has finished
+        if (isAttacking && attackDurationTimer <= 0)
         {
-            // Decrement the attack cooldown timer
-            attackCooldownTimer -= Time.deltaTime;
-            if (attackCooldownTimer <= 0)
-            {
-                isAttacking = false;
-            }
+            isAttacking = false;
+        }
+
+        // Check if the cooldown has elapsed
+        if (isCooldown && attackCooldownTimer <= 0)
+        {
+            isCooldown = false;
+            speed = originalSpeed; // Restore the original speed
         }
     }
 }
---
+++ Assets/Scripts/SwordAttack.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAttack : MonoBehaviour
+{
+		Collider2D swordCollider;
+	
+		private void Start() {
+			swordCollider = GetComponent;
+}
+
+
+
+
+}
